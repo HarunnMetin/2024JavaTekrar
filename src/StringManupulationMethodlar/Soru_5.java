7f146adda6172ae29fe516ccfd35425758e1eb10
HarunnMetin <mtnmusa07@gmail.com>
2024-01-13 05:09:30 +0300

    soru5

---
+++ src/StringManupulationMethodlar/Soru_5.java
@@ -0,0 +1,16 @@
+package StringManupulationMethodlar;
+
+import java.util.Locale;
+
+public class Soru_5 {
+
+    public static void main(String[] args) {
+
+       // Bir cümle içindeki tüm karakterleri nasıl büyük harfe çevirebilirsin?
+
+        String cumle = "ben her ne kadar zor olursaolsun basaracagim";
+
+        String buyukHarf = cumle.toUpperCase();
+        System.out.println(buyukHarf);
+    }
+}
