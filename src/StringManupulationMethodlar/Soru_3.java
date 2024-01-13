670f06ac961823193747e434f4d85990f62248d7
HarunnMetin <mtnmusa07@gmail.com>
2024-01-13 03:11:11 +0300

    manipulation

---
+++ src/StringManupulationMethodlar/Soru_3.java
@@ -0,0 +1,15 @@
+package StringManupulationMethodlar;
+
+public class Soru_3 {
+
+    public static void main(String[] args) {
+
+        String sayi1 = "123";
+        String sayi2 = "456";
+
+        // Bu iki sayıyı birleştirerek tek bir string oluşturunuz.
+
+        String birlesikStr = sayi1 + sayi2;
+        System.out.println(birlesikStr);
+    }
+}
