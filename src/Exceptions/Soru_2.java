c8b5b0c285f1426d52a7efb70fcd6a849af11df6
HarunnMetin <mtnmusa07@gmail.com>
2024-01-15 05:52:56 +0300

    sorular

--- src/Exceptions/Soru_2.java
+++ src/Exceptions/Soru_2.java
@@ -1,8 +1,34 @@
 package Exceptions;
 
+import java.util.Arrays;
+
 public class Soru_2 {
 
     public static void main(String[] args) {
 
+        //Bir dizide belirli bir indekse erişmeye çalışın.
+        // Ancak indeks, dizinin boyutunu aşıyorsa bir istisna fırlatın.
+
+
+
+        try {
+            int[] dizi = {1,2,3,4,5};
+            int index = 3;
+
+            if (index >= dizi.length || index < 0){
+
+                throw new  IndexOutOfBoundsException("Index dizi sinirlarinin disinda");
+            }
+            System.out.println(Arrays.toString(dizi));
+        }
+        catch (IndexOutOfBoundsException e){
+            System.out.println("Hata!!" + e.getMessage());
+        }
+        catch (Exception e){
+            System.out.println("Bilinmeyen hata!!!!!!" + e.getMessage());
+        }
+        finally {
+            System.out.println("yapabidigimin farkinda oldugum icin birakamiyorum");
+        }
     }
 }
