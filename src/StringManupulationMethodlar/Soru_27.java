dd9ac3b97f769d18515dbfae4f21899bd28185e8
HarunnMetin <mtnmusa07@gmail.com>
2024-02-20 15:25:40 +0300

    java

--- src/StringManupulationMethodlar/Soru_27.java
+++ src/StringManupulationMethodlar/Soru_27.java
@@ -15,12 +15,18 @@ public class Soru_27 {
         Scanner scaner = new Scanner(System.in);
         System.out.println("Lutfen bir kelime girin");
         String kelime = scaner.nextLine();
-        if (kelime.length() %2 == 0){
-            System.out.println();
+        if (kelime.length() % 2 == 0) {
+            // String uzunluğu çift ise ortasına :) ekleyin
+            int ortaIndek = kelime.length() / 2;
+            String result = kelime.substring(0, ortaIndek) + ":)" + kelime.substring(ortaIndek);
+            System.out.println("Sonuç: " + result);
+        } else {
+            // String uzunluğu tek ise ortadaki harfi silin ve yerine :( ekleyin
+            int middleIndex = kelime.length() / 2;
+            String result = kelime.substring(0, middleIndex) + kelime.substring(middleIndex + 1);
+            System.out.println("Sonuç: " + result);
+
+
         }
-
-
-
-
     }
 }
